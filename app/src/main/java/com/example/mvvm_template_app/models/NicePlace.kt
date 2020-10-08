de24ea2fc8225d79bc7608592f30492639d57375
Jeff Arce (MacBook Air) <jeffandrew.s.arce@gmail.com>
2020-10-08 15:38:51 +0800

    Remove unused Classes.

--- app/src/main/java/com/example/mvvm_template_app/models/NicePlace.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mvvm_template_app.models
-
-data class NicePlace(
-    var imageUrl: String,
-    var title: String) {
-
-    constructor(): this("", "")
-}
--- app/src/main/java/com/example/mvvm_template_app/models/Player.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mvvm_template_app.models
-
-data class Player(
-    var imageUrl: String,
-    var title: String) {
-
-    constructor(): this("", "")
-}
