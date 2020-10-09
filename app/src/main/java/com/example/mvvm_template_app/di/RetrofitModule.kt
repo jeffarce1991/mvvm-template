980552109cfcc7e7e6c66cf367c45b55ea0c0ffa
Jeff Arce (MacBook Air) <jeffandrew.s.arce@gmail.com>
2020-10-09 14:14:48 +0800

    Add RetrofitModule.

---
+++ app/src/main/java/com/example/mvvm_template_app/di/RetrofitModule.kt
@@ -0,0 +1,46 @@
+package com.example.mvvm_template_app.di
+
+import com.example.mvvm_template_app.api.ApiService
+import com.example.mvvm_template_app.models.User
+import com.example.mvvm_template_app.models.UserDto
+import com.example.mvvm_template_app.utils.Constants
+import com.example.mvvm_template_app.utils.EntityMapper
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideGsonBuilder(): Gson {
+        return GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit.Builder): ApiService {
+        return retrofit
+            .build()
+            .create(ApiService::class.java)
+    }
+}
